fd73d6e93427b6284e4d9ae5e313b9677ad9dd3d
ezrapasha <ezra.pasha@ui.ac.id>
2023-12-08 21:53:49 +0700

    added move

--- Scene/MainCharacter/MovementCharacter.gd
+++ Scene/MainCharacter/MovementCharacter.gd
@@ -1,23 +1,47 @@
 extends CharacterBody2D
 
-@export var speed = 400
+@export var speed = 700
+@export var max_speed = 2000
 @export var gravity = 40
 @export var jump = 800
 
-func get_input():
+var curr_speed : float= 0
+var speed_lerp : float = 0
+
+func get_input(delta):
 	var input_direction = Input.get_axis("left", "right")
-	velocity.x = input_direction * speed
+	
+	# if !Input.is_action_pressed("slide"):
+	# 	curr_speed = input_direction * speed
+	if Input.is_action_just_pressed("slide"):
+		curr_speed += input_direction * speed * 0.5
+	
+	if !Input.is_action_pressed("slide"):
+		if !is_on_floor():
+			#if abs(curr_speed) <= speed:
+			curr_speed += input_direction * speed * delta * 2
+			curr_speed = min(curr_speed, max_speed)
+			curr_speed = max(curr_speed, -max_speed)
+		else:
+			curr_speed = input_direction * speed
+		
+	velocity.x = curr_speed
+	
+	print(velocity.x)
 
 func _physics_process(delta):
+	
 	if !is_on_floor():
 		velocity.y += gravity
 		if velocity.y > 800:
 			velocity.y = 800
-		print(velocity.y)
-		print(global_position)
 
+	if Input.is_action_pressed("slide"):
+		curr_speed -= velocity.normalized().x * 10
+	
 	if Input.is_action_just_pressed("jump") && is_on_floor():
 		velocity.y = -jump
+		# velocity.x = Input.get_axis("left", "right") * speed
 	
-	get_input()
+	get_input(delta)
 	move_and_slide()
