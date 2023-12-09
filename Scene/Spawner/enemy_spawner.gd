522c1cb3dce502c4bfc022f189137dd5c3a4f2d7
Abdul Rafi <abdulrafi907@gmail.com>
2023-12-10 00:22:19 +0700

    add feat

---
+++ Scene/Spawner/enemy_spawner.gd
@@ -0,0 +1,39 @@
+extends Node2D
+class_name EnemySpawner
+
+@export var enemy_list : Array[PackedScene]
+@export var position_list : Array[Node2D]
+
+var rng = RandomNumberGenerator.new()
+var avail_position = []
+var spawned_object = []
+signal got_food
+
+func _ready():
+	reset_spanwer()
+		
+func spawn_enemy(number_of_enemy):
+	for i in range(number_of_enemy):
+		if(len(avail_position) == 0):
+			break
+		var get_index : int = rng.randi_range(0, avail_position.size()-1)
+		var random_position = position_list[avail_position[get_index]]
+		avail_position.remove_at(get_index)
+		
+		var get_index_enemy = rng.randi_range(0, enemy_list.size() - 1)
+		var created_enemy = enemy_list[get_index_enemy].instantiate()
+		add_child(created_enemy)
+		
+		created_enemy.global_position = random_position.global_position
+		spawned_object.append(created_enemy)
+
+
+func reset_spanwer():
+	for i in range(spawned_object.size()):
+		if(is_instance_valid(spawned_object[i])):
+			spawned_object[i].queue_free()
+			
+	spawned_object.clear()
+	avail_position.clear()
+	for i in range(len(position_list)):
+		avail_position.append(i)
